da6a2ff85d2f244c0813007b85c965c11eee3939
Ouail-01 <ouaillaarabi@gmail.com>
2023-08-25 21:11:51 +0100

    Add users API

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+    render json: @courses
+  end
+
+  def show
+    @course = Course.find(params[:id])
+
+    render json: @course
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      render json: @course, status: :created
+    else
+      render json: { errors: @course.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :description, :image, :price, :user_id)
+  end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::UsersController < ApplicationController
+  def start
+    return unless request.content_type == 'application/json'
+    return unless request['message'] == 'start'
+
+    msg = { message: 'welcome' }
+    render json: msg, status: :ok
+  end
+
 end
