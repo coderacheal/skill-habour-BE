9bcde825296d7790d00b62b368a0f23a04b4dc43
Ouail-01 <ouaillaarabi@gmail.com>
2023-08-25 21:12:43 +0100

    Updates

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,18 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :courses 
+  # resources :courses 
   
-  resources :user do
-    resources :reservations do
+  # resources :user do
+  #   resources :reservations do
+  #   end
+  # end
+
+  namespace :api do
+    namespace :v1 do
+      post 'users/new_session', to: 'users#new_session'
+      post 'users/start', to: 'users#start'
+      resources :courses
     end
   end
 
