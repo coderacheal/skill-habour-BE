a8fce9e66b00907abbaa9952b2305217b4e41635
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-08-26 12:21:01 +0500

    Fix rubocop errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,14 +2,16 @@ class Api::V1::UsersController < ApplicationController
   def start
     return unless request.content_type == 'application/json'
     return unless request['message'] == 'start'
-​
+
+    ​
     msg = { message: 'welcome' }
     render json: msg, status: :ok
   end
-​
+  ​
   def new_session
     return unless request.content_type == 'application/json'
-​
+
+    ​
     user = User.where(username: request['username'])
     if user.empty?
       new_user = User.create!(user_params)
@@ -27,7 +29,7 @@ class Api::V1::UsersController < ApplicationController
     end
     render json: msg, status: :ok
   end
-​
+  ​
   def user_params
     params.require(:user).permit(:username)
   end
