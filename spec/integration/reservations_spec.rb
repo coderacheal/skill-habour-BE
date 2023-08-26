cf33b4f4b615e72c59928b8f2f89fdf9aa57f571
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-08-26 12:21:24 +0500

    Fix linter errors

--- spec/integration/reservations_spec.rb
+++ spec/integration/reservations_spec.rb
@@ -3,10 +3,11 @@ require 'swagger_helper'
 RSpec.describe 'api/v1/reservations', type: :request do
   before(:each) do
     @user = User.create(username: 'Ouail')
-    @course = Course.create(name: 'course', description: 'description', image: 'https://www.wcrf-uk.org/wp-content/uploads/2021/06/588595864r-LS.jpg', price: 456, id: 1)
-    @reservation = Reservation.create(city: "City Name", course_name: @course.id, reservation_date: Date.today)
+    @course = Course.create(name: 'course', description: 'description',
+                            image: 'https://www.wcrf-uk.org/wp-content/uploads/2021/06/588595864r-LS.jpg', price: 456, id: 1)
+    @reservation = Reservation.create(city: 'City Name', course_name: @course.id, reservation_date: Date.today)
   end
-​
+  ​
   path '/api/v1/reservations' do
     get('list reservations') do
       parameter name: 'user_id', in: :path, type: :integer, required: true
@@ -33,7 +34,7 @@ RSpec.describe 'api/v1/reservations', type: :request do
       end
     end
   end
-​
+  ​
   path '/api/v1/reservations' do
     post('create reservation') do
       consumes 'application/json'
@@ -51,12 +52,12 @@ RSpec.describe 'api/v1/reservations', type: :request do
       response(201, 'successful') do
         schema type: :object,
                properties: {
-                reservation_date: { type: :date },
+                 reservation_date: { type: :date },
                  created_at: { type: :string, format: 'date-time' },
                  updated_at: { type: :string, format: 'date-time' }
                },
-               required: %w[ reservation_date ]
-​
+               required: %w[reservation_date]
+        ​
         run_test!
       end
     end
