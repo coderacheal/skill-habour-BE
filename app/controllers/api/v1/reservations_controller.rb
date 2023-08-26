c808f9749d6129ba09c06d3e418597c8c5fdce02
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-08-26 12:20:32 +0500

    Fix rubocop errors

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,28 +1,30 @@
 class Api::V1::ReservationsController < ApplicationController
-    before_action :set_reservation, only: %i[show update destroy]
+  before_action :set_reservation, only: %i[show update destroy]
   ​
-    def create
-      @reservation = Reservation.new(reservation_params)
-      if @reservation.save
-        render json: @reservation, status: :created
-      else
-        render json: @reservation.errors, status: :unprocessable_entity
-      end
-    end
-  ​
-    def index
-      @user_reservations = Reservation.where(reservation_id: params[:user_id])
-      render json: @user_reservations
-    end
-    
-  ​
-    private
-  ​
-    def set_reservation
-      @reservation = Reservation.find(params[:id])
-    end
-  ​
-    def reservation_params
-      params.require(:reservation).permit(:course_id, :reservation_date, :city_id, :reservation_id)
+  def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      render json: @reservation, status: :created
+    else
+      render json: @reservation.errors, status: :unprocessable_entity
     end
   end
+  ​
+  def index
+    @user_reservations = Reservation.where(reservation_id: params[:user_id])
+    render json: @user_reservations
+  end
+
+  ​
+
+  private
+
+  ​
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+  ​
+  def reservation_params
+    params.require(:reservation).permit(:course_id, :reservation_date, :city_id, :reservation_id)
+  end
+end
