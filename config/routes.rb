9c753efc42dd9c23ae5a46196cb43de669295f13
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-08-26 12:10:55 +0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+​
   # Defines the root path route ("/")
   # root "articles#index"
-
+​
   # resources :courses 
   
   # resources :user do
   #   resources :reservations do
   #   end
   # end
-
+​
   namespace :api do
     namespace :v1 do
       post 'users/new_session', to: 'users#new_session'
       post 'users/start', to: 'users#start'
       resources :courses
+      resources :reservations
     end
   end
-
+​
 end
