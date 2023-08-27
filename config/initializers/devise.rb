08230efcb7ec52455290a6b43859a109581a9f6b
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-08-27 16:46:30 +0000

    Remove `config.jwt` from inner devise block

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -313,16 +313,18 @@ Devise.setup do |config|
 
   # config.navigational_formats = [:json]
   # config.authentication_keys = [:email, :username]
-  config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
-    jwt.dispatch_requests = [
-      ['POST', %{^/users/sign_in$}]
-    ]
+  
+end
 
-    jwt.dispatch_requests = [
-      ['DELETE', %{^/users/sign_OUT}]
-    ]
+config.jwt do |jwt|
+  jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
+  jwt.dispatch_requests = [
+    ['POST', %{^/users/sign_in$}]
+  ]
 
-    jwt.expiration_time = 120.minutes.to_i
-  end
+  jwt.dispatch_requests = [
+    ['DELETE', %{^/users/sign_out}]
+  ]
+
+  jwt.expiration_time = 120.minutes.to_i
 end
