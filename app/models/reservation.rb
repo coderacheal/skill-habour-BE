cfe78248e66eb48b3b346fac786cf4fa9e1a8c18
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-08-30 21:05:13 +0500

    Update reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,11 @@
 class Reservation < ApplicationRecord
+    belongs_to :user
+    belongs_to :Course
+  
+    # validations
+    validates :city, presence: true
+    validates :course_name, presence: true
+    validates :reservation_date, presence: true
+    validates :reservation_id, presence: true
+    validates :user_id, presence: true
 end
