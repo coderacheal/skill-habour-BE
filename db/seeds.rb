ed7d2264249ae73ea2fc437d895b8a9dab2eb0a9
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-09-01 14:14:50 +0000

    Push missing push code

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@
 
 
 
-Course.create(name: 'Cooking', description: 'Discover the secrets of gourmet meals. From beginner basics to gourmet recipes.', image: 'cooking',  price: 20)
-Course.create(name: 'Pottery', description: 'Dive into the world of clay and ceramics, where you\'ll learn to craft beautiful pottery pieces', image: 'pottery', price: 20)
-Course.create(name: 'Music', description: 'Explore the magic of music, from playing instruments to mastering music theory', image: 'music', price: 20)
-Course.create(name: 'Swimming', description: 'Make a splash! Dive into the world of aquatic fitness and water safety', image: 'swimming', price: 20)
+Course.create(name: 'Cooking', description: 'Discover the secrets of gourmet meals. From beginner basics to gourmet recipes.', image: 'https://raw.githubusercontent.com/coderacheal/skill-habour-fe/dev/src/assets/cooking.jpg',  price: 20)
+Course.create(name: 'Pottery', description: 'Dive into the world of clay and ceramics, where you\'ll learn to craft beautiful pottery pieces', image: 'https://raw.githubusercontent.com/coderacheal/skill-habour-fe/dev/src/assets/pottery.jpg', price: 20)
+Course.create(name: 'Music', description: 'Explore the magic of music, from playing instruments to mastering music theory', image: 'https://raw.githubusercontent.com/coderacheal/skill-habour-fe/dev/src/assets/music.jpg', price: 20)
+Course.create(name: 'Swimming', description: 'Make a splash! Dive into the world of aquatic fitness and water safety', image: 'https://raw.githubusercontent.com/coderacheal/skill-habour-fe/dev/src/assets/swimming.jpg', price: 20)
 
-User.create(username: 'Racheal', email: 'coderacheal@gmail.com', password: '12345')
+# User.create(username: 'Racheal', email: 'coderacheal@gmail.com', password: '12345')
