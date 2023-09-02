98cd8ea900f09b67c33edced095e637f276e1e16
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-09-02 08:31:02 +0000

    Remove `credentials.yml.enc` from git tracking

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,3 @@
 class Reservation < ApplicationRecord
+  has_many courses
 end
--- db/migrate/20230821133554_add_foreign_key_reference.rb
+++ db/migrate/20230821133554_add_foreign_key_reference.rb
@@ -1,6 +1,6 @@
 class AddForeignKeyReference < ActiveRecord::Migration[7.0]
   def change
-      add_reference :reservations, :reservation, foreign_key: { to_table: :reservations }
+    add_reference :reservations, :course_id, foreign_key: { to_table: :courses }
   end
 end
 
