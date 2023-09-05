c236cc532f093f3e2349da7253fef119b2daf21e
brhanuhailu <hailubrhanu29@gmail.com>
2023-09-05 05:07:08 -0700

    applicatiion controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
 
+  before_action :authenticate_user!
+  before_action :set_current_user
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+  def set_current_user
+    Ability.new(current_user)
   end
 end
